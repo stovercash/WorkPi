bb7fd27ca17fddb923d26061bfddc98067b4152c
STOvercash <tyler.overcash.com>
2017-02-22 13:22:12 -0500

    added html page to display vso changes in a graient grid

--- html/index.php
+++ html/index.php
@@ -1,10 +1,14 @@
 <html>
 <head>
 </head>
-<body>
+<body style="background-color: #000000; margin: 0; padding: 0; font-family: Consolas">
+<div style="width: 100%; height: 100px; background-color: #C00000; padding: 20px; font-size: 80px;">
+	AVALON
+</div>
+<div style="width: 288px; height: 120px; background-color: white; margin: 20px; padding: 20px; border-color: white; border-radius: 10px 35px">
 <?php
 include 'vsocheckintable.php';
 ?>
-
+</div>
 </body>
 </html>
--- html/vsocheckintable.php
+++ html/vsocheckintable.php
@@ -1,20 +1,71 @@
 <?php
+$mysecrets = json_decode(file_get_contents("/home/pi/dev/WorkPi/setup/secrets.json"), true);
+
+$bordersize = 2;
+$boxsize = 20;
 $ytotal = 5;
 $xtotal = 12;
+$maxchanges = 50;
+
 $y = 1;
+$total_days = $ytotal * $xtotal;
+
+$mysqli = new mysqli('localhost',$mysecrets["sql"]["user"],$mysecrets["sql"]["pass"],$mysecrets["sql"]["dbname"]);
+$sql_stmt = $mysqli->prepare("SELECT IFNULL(SUM(NoOfObjects),0) AS Changes FROM VSOCheckIn WHERE DateCheckedIn BETWEEN ? AND ?");
+
 while($y <= $ytotal)
 {
 	$x = 1;
 	while($x <= $xtotal)
 	{
-		echo '<div style="height: 20px; width: 20px; margin: 2px 2px 2px 2px; background-color: #ff0000; float: left; ';
+		$subtract_days = $total_days - ((($x - 1) * $ytotal) + $y);
+		$this_date = new DateTime();
+		$this_date = $this_date->modify("-$subtract_days days");
+		$this_month = intval($this_date->format('m'));
+
+		$this_start_time = $this_date->format('Y-m-d') . " 00:00:00.00";
+		$this_end_time = $this_date->format('Y-m-d') . " 23:59:59.999";
+		$sql_stmt->bind_param("ss",$this_start_time,$this_end_time);
+		$sql_stmt->execute();
+		$sql_stmt->bind_result($changes);
+		$sql_stmt->fetch();
+
+		echo '<div style="height: ' . $boxsize . 'px; width: ' . $boxsize . 'px; border-width: ' . $bordersize . 'px; border-style: solid; background-color: #ff0000; float: left; ';
+		if (($this_month % 2) == 0)
+		{
+			echo 'border-color: #D9D9D9; ';
+			$this_gray = 217;
+		}
+		else
+		{
+			echo 'border-color: #E5E5E5; ';
+			$this_gray = 229;
+		}
+		if ($changes > $maxchanges)
+		{
+			$changes = $maxchanges;
+		}
+		$changes = round($this_gray * (($maxchanges - $changes) / $maxchanges));
+		$hexchanges = dechex($changes);
+		if ($changes < 16)
+		{
+			$hexchanges = '0' . dechex($changes);
+		}
+		else
+		{
+			$hexchanges = dechex($changes);
+		}
+		echo 'background-color: #' . dechex($this_gray) . $hexchanges . $hexchanges . '; ';
 		if ($x == 1)
 		{
 			echo 'clear: left';
 		}
+
+
 		echo '">&nbsp</div>';
 		$x++;
 	}
 	$y++;
 }
+$sql_stmt->close();
 ?>
