1515c8e7fc57f1de4941a9d93723ef447c2b9cea
STOvercash <tyler.overcash.com>
2017-04-03 15:59:48 -0400

    Enhanced cosmetics of avalon index page

--- html/index.php
+++ html/index.php
@@ -7,12 +7,36 @@ include 'vsocheckinpie.php';
 <body style="background-color: #000000; margin: 0; padding: 0; font-family: Consolas">
 <div style="width: 100%; height: 140px; margin: 0; padding: 0">
 	<div style="width: auto; height: 100px; background-color: #C00000; padding: 20px; font-size: 80px; float: left">
-		<span style="text-shadow: 0 0 10px #C0C0C0, 0 0 20px #C0C0C0, 0 0 50px #C0C0C0, 0 0 100px #C0C0C0;">
+		<span style="text-shadow: 0 0 10px #000000;">
 			AVALON
 		</span>
 	</div>
 	<div style="width: 75px; height: 140px; background-color: #C00000; padding: 0; margin: 0; float: right; background-image: url('WorkPiBanner1.png')"></div>
-	<div style="background-color: #C00000; height: 140px;"></div>
+	<div style="background-color: #C00000; height: 140px; overflow: hidden;">
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1010100010 &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp 1 &nbsp &nbsp 1 &nbsp 101 &nbsp &nbsp 01 &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 100000101 &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 01 &nbsp &nbsp &nbsp &nbsp &nbsp 1001 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 100000 1 &nbsp 1 &nbsp 
+	&nbsp 010 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1010010001 &nbsp 10 &nbsp &nbsp &nbsp &nbsp 01 &nbsp &nbsp 001 &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp 010 &nbsp &nbsp &nbsp &nbsp &nbsp 111001001 &nbsp &nbsp 1 &nbsp &nbsp &nbsp &nbsp 101 11 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp 1 &nbsp 11100101011100010 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 11 &nbsp &nbsp  1 &nbsp  01 &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp 1 11011110 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1 &nbsp &nbsp &nbsp &nbsp &nbsp 010 &nbsp 1 &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 01 &nbsp 100 &nbsp &nbsp &nbsp &nbsp 111 &nbsp &nbsp &nbsp &nbsp 000 &nbsp &nbsp &nbsp 
+	00011 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1001 &nbsp 1 &nbsp &nbsp &nbsp  01 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1011011 &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp 101 &nbsp &nbsp &nbsp 101 &nbsp &nbsp 110 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 00000001 &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 0 101011101110000101111000 &nbsp &nbsp 
+	&nbsp &nbsp &nbsp 1 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 010 &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1100 &nbsp &nbsp 011 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 0 &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1100110 &nbsp 1 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 01110 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 01 &nbsp 01 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1101000111 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 010 &nbsp 0100 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1111 
+	&nbsp &nbsp &nbsp 1110 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 11 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 111100011 &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp 11 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1 &nbsp 00111 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 000 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 00 &nbsp &nbsp 
+	&nbsp &nbsp 1111000011011 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 01011 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+	00011 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1001 &nbsp 1 &nbsp &nbsp &nbsp  01 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1011011 &nbsp &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 1010100010 &nbsp &nbsp &nbsp 
+	&nbsp &nbsp &nbsp &nbsp &nbsp 1010 &nbsp &nbsp &nbsp 10101 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 2
+	</div>
 </div>
 <div style="width: 288px; height: 120px; background-color: white; margin: 20px; padding: 20px; border-color: white; border-radius: 10px 35px; float: left">
 <?php
